4d114cd427637e9b9bcbba25ac86d53acf1d6cbb
iproman <itechman@yandex.ru>
2019-12-25 11:49:41 +0500

    Add homeComponent to app.module.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -11,6 +11,7 @@ import {PostService} from './services/post.service';
 import {HttpClientModule} from '@angular/common/http';
 import {PostFormComponent} from './components/post-form/post-form.component';
 import { AppRoutingModule } from './app-routing.module';
+import { HomeComponent } from './components/home/home.component';
 
 @NgModule({
   declarations: [
@@ -19,7 +20,8 @@ import { AppRoutingModule } from './app-routing.module';
     UsersComponent,
     NavbarComponent,
     PostsComponent,
-    PostFormComponent
+    PostFormComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
