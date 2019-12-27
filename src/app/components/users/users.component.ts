86f8b1cfe329cf95ffd67a4326957267197ae98f
iproman <itechman@yandex.ru>
2019-12-27 05:44:22 +0500

    Move addUser to service, add onSubmit for new users.

--- src/app/components/users/users.component.ts
+++ src/app/components/users/users.component.ts
@@ -1,18 +1,28 @@
-import {Component, OnInit} from '@angular/core';
+import {Component, OnInit, ViewChild} from '@angular/core';
 import {User} from '../../models/User';
+import {UserService} from '../../services/user.service';
 
 @Component({
   selector: 'app-users',
   templateUrl: './users.component.html',
   styleUrls: ['./users.component.css']
 })
-export class UsersComponent implements OnInit {
 
+export class UsersComponent implements OnInit {
+  user: User = {
+    firstName: '',
+    lastName: '',
+    email: '',
+  };
   users: User[];
   showExtended = true;
   loaded = false;
+  enableAdd = false;
+  showUserForm: false;
+  @ViewChild('userForm', {static: false}) form: any;
+  data: any;
 
-  constructor() {
+  constructor(private userService: UserService) {
   }
 
   ngOnInit() {
@@ -25,7 +35,16 @@ export class UsersComponent implements OnInit {
     });
   }
 
-  addUser(user: User) {
-    this.users.push(user);
+  onSubmit({value, valid}: { value: User, valid: boolean }) {
+    if (!valid) {
+      console.log('Form is not valid');
+    } else {
+      value.isActive = true;
+      value.registered = new Date();
+      value.hide = true;
+      this.userService.addUser(value);
+
+      this.form.reset();
+    }
   }
 }
